6f3eeb1b1b5777c3c4c0329d442913d357d2d284
Roald Ong <roald.ong@gmail.com>
2024-04-26 15:36:08 +0800

    Add in scan publisher and subscriber

--- include/robosense_filter/ring_filter.h
+++ include/robosense_filter/ring_filter.h
@@ -1,15 +1,43 @@
 #ifndef RING_FILTER_H
 #define RING_FILTER_H
-
+#include <pcl/pcl_config.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <ros/ros.h>
+
+#if PCL_VERSION_COMPARE(>, 1, 9, 0)
+#include <pcl-1.10/pcl/filters/impl/filter.hpp>
+#endif
+
+#if PCL_VERSION_COMPARE(<, 1, 9, 0)
+#include <pcl-1.8/pcl/filters/impl/filter.hpp>
+#endif
+
+struct EIGEN_ALIGN16 Point {
+  PCL_ADD_POINT4D;
+  std::uint16_t intensity;
+  double timestamp;
+  std::uint16_t ring;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+    Point, (float, x, x)(float, y, y)(float, z, z)(uint8_t, intensity,
+                                                   intensity)(
+               double, timestamp, timestamp)(std::uint16_t, ring, ring))
 
 class RingFilter {
  public:
   RingFilter(ros::NodeHandle nh, ros::NodeHandle pnh);
 
  private:
+  void scanCallback(const sensor_msgs::PointCloud2ConstPtr &msg);
   ros::NodeHandle m_nh;
   ros::NodeHandle m_pnh;
+
+  ros::Subscriber m_scan_sub;
+  ros::Publisher m_scan_pub;
 };
 
 #endif  // RING_FILTER_H
--- src/ring_filter.cpp
+++ src/ring_filter.cpp
@@ -1,7 +1,41 @@
 #include <robosense_filter/ring_filter.h>
+
 #include <iostream>
 
 RingFilter::RingFilter(ros::NodeHandle nh, ros::NodeHandle pnh)
     : m_nh(nh), m_pnh(pnh) {
-  std::cout << "RingFilter constructor called" << std::endl;
+  m_scan_sub =
+      m_nh.subscribe("/rslidar_points", 10, &RingFilter::scanCallback, this);
+
+  m_scan_pub =
+      m_nh.advertise<sensor_msgs::PointCloud2>("/ring_filtered_points", 10);
+}
+
+void RingFilter::scanCallback(const sensor_msgs::PointCloud2ConstPtr& msg) {
+  pcl::PointCloud<Point>::Ptr cloud(new pcl::PointCloud<Point>);
+  pcl::PointCloud<Point>::Ptr output_cloud(new pcl::PointCloud<Point>);
+
+  pcl::fromROSMsg(*msg, *cloud);
+
+  std::cout << pcl::getFieldsList(*msg) << std::endl;
+  for (auto& it : cloud->points) {
+    if (it.ring == 1 || it.ring == 2 || it.ring == 3 || it.ring == 4 ||
+        it.ring == 5 || it.ring == 6 || it.ring == 8 || it.ring == 9 ||
+        it.ring == 11 || it.ring == 12 || it.ring == 14 || it.ring == 15 ||
+        it.ring == 16 || it.ring == 17 || it.ring == 18 || it.ring == 19) {
+      output_cloud->points.push_back(it);
+    }
+  }
+
+  output_cloud->header = cloud->header;
+  output_cloud->height = 1;
+  output_cloud->width = output_cloud->points.size();
+  output_cloud->is_dense = cloud->is_dense;
+  output_cloud->sensor_origin_ = cloud->sensor_origin_;
+  output_cloud->sensor_orientation_ = cloud->sensor_orientation_;
+
+  sensor_msgs::PointCloud2 output_msg;
+  pcl::toROSMsg(*output_cloud, output_msg);
+  output_msg.header = msg->header;
+  m_scan_pub.publish(output_msg);
 }
